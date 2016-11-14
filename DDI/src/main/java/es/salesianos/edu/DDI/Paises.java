1b6d1c1409de80413847da3f50545446bf3eb293
Julio Bernad <Julio Bernad>
2016-11-14 18:29:17 +0100

    Primer commit del Servlet
    Signed-off-by: Julio Bernad <Julio Bernad>

---
+++ DDI/src/main/java/es/salesianos/edu/DDI/Paises.java
@@ -0,0 +1,19 @@
+package es.salesianos.edu.DDI;
+
+public class Paises {
+	private String nombre;
+	private String lengua;
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getLengua() {
+		return lengua;
+	}
+	public void setLengua(String lengua) {
+		this.lengua = lengua;
+	}
+}
