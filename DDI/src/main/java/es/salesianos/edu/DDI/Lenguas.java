8836cf48771afa08b138026e2877c1fca1749995
Julio Bernad <Julio Bernad>
2016-11-28 22:06:23 +0100

    Signed-off-by: Julio Bernad <Julio Bernad>

---
+++ DDI/src/main/java/es/salesianos/edu/DDI/Lenguas.java
@@ -0,0 +1,12 @@
+package es.salesianos.edu.DDI;
+
+public class Lenguas {
+	private String lengua;
+	
+	public String getLengua() {
+		return lengua;
+	}
+	public void setLengua(String lengua) {
+		this.lengua = lengua;
+	}
+}
